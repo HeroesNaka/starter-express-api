f728b7df24937495abc3f65422ac28be9f9224a1
Jon Curtis <joncurtis312@gmail.com>
2023-11-01 11:39:05 -0400

    Your commit message

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var app = express();
 // view engine setup
 
 app.set('view engine', 'ejs');
-
 app.set('views', path.join(__dirname, 'views'));
-
 app.use('/img', express.static(__dirname + 'views/img'))
 app.use(express.static(path.join(__dirname, 'views')));
 
@@ -34,6 +32,11 @@ app.use(express.json())
 app.use(cors())
 
 
+//Router to render hompage
+app.get('/', (req,res)=> {
+  res.render('index.ejs')
+})
+
 app.post('/email', (req,res) => {
   const {collectUserName, collectPassword} = req.body
   console.log('Data:', req.body)
@@ -47,26 +50,5 @@ app.post('/email', (req,res) => {
   })
 
 })
-
-//Router to render html
-app.get('/', (req,res)=> {
-  res.render('index.ejs')
-})
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
